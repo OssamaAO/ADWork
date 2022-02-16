6e1fa6922a7c27958839ec2005a8dc13c814dcf4
OssamaAO <oaouadsno.al19@emiliojimeno.edu.es>
2022-02-16 19:21:52 +0100

    Update

---
+++ src/adworkfinal/ADWorkFinal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adworkfinal;
+
+/**
+ *
+ * @author alumno1
+ */
+public class ADWorkFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ESKETIT");
+    }
+    
+}
